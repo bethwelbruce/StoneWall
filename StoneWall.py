57a8c5017281723473c7c7065a4f602572059201
BETHWEL NELSON OTIENO <bethwelbruce@gmail.com>
2022-11-20 01:19:14 +0300

    Add files via upload

---
+++ StoneWall.py
@@ -0,0 +1,18 @@
+def solution(H):
+    last=0
+    c=0
+    S=[]
+    for i in range(0,len(H)):
+        if(H[i]>last):
+            last=H[i]
+            c+=1
+            S.append(H[i])
+        elif(H[i]<last):
+            while(len(S)>0 and H[i]<S[-1]):
+                S.pop()
+            if(len(S)==0 or H[i]!=S[-1]):
+                c+=1
+                S.append(H[i])
+                last=H[i]
+    return c
+    pass
